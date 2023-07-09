f2c4bf2c57ebb9697680ac195e227f75315f24d9
Lynn Zhou <lynnreload@163.com>
2023-07-09 14:41:51 +0800

    add List method to orm

--- orm/session/session.go
+++ orm/session/session.go
@@ -3,6 +3,7 @@ package session
 import (
 	"database/sql"
 	"errors"
+	"reflect"
 	"strings"
 
 	"github.com/lz-nsc/cupcake/orm/log"
@@ -109,4 +110,40 @@ func (s *Session) Insert(instances ...interface{}) (int64, error) {
 	}
 
 	return result.RowsAffected()
+}
+
+// List accept array of struct as parameter and return all the record of related table
+func (s *Session) List(arr interface{}) error {
+	list := reflect.Indirect(reflect.ValueOf(arr))
+	elemType := list.Type().Elem()
+
+	// Generate a new instance with given list's element type, then pass to orm session
+	err := s.Model(reflect.New(elemType).Elem().Interface())
+	if err != nil {
+		return err
+	}
+	schema := s.Schema()
+	s.statement.Set(SELECT, schema.Name, schema.FieldNames)
+	sql, vars := s.statement.Build(SELECT)
+
+	rows, err := s.Raw(sql, vars...).QueryRows()
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		elem := reflect.New(elemType).Elem()
+
+		var values []interface{}
+		for _, name := range schema.FieldNames {
+			values = append(values, elem.FieldByName(name).Addr().Interface())
+		}
+
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+
+		list.Set(reflect.Append(list, elem))
+	}
+	return nil
 }
